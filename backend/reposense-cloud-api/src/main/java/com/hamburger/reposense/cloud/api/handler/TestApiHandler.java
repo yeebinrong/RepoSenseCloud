10496a1a5ff7164c8c2138237cdabc3ef999a6a1
yee_B <yeebinrong@gmail.com>
2024-08-29 03:18:46 +0800

    HAMB-4 wip set up dev env

---
+++ backend/reposense-cloud-api/src/main/java/com/hamburger/reposense/cloud/api/handler/TestApiHandler.java
@@ -0,0 +1,8 @@
+package com.hamburger.reposense.cloud.api.handler;
+
+@Log4j2
+public class TestApiHandler extends AbstractApiGatewayHandler {
+    public static void main(String[] args) {
+        log.info("Hello world!");
+    }
+}
---
+++ backend/reposense-cloud-common/src/main/java/com/hamburger/reposense/cloud/common/handler/AbstractApiGatewayHandler.java
@@ -0,0 +1,7 @@
+package com.hamburger.reposense.cloud.common.handler;
+
+public class AbstractApiGatewayHandler {
+    public static void main(String[] args) {
+        System.out.println("Hello world 2!");
+    }
+}
