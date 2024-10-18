6e6b9732b0fddef427837e998733313740c896e7
gwee melvin <gwee_melvin@live.com>
2024-10-18 15:40:00 +0800

    added divider for page

--- frontend/src/components/CreateJobComponent/CreateJobComponent.js
+++ frontend/src/components/CreateJobComponent/CreateJobComponent.js
@@ -17,6 +17,15 @@ class CreateJobComponent extends React.Component {
                     <div className = "dotted-line"/>
                     <img src = {PageIcon} alt="Page Icon" className="page-icon2"/>
                 </span>
+                <span className= "create-job-input-container">
+                    <div className="create-job-input-1">
+                        <h3>test2</h3>
+                    </div>
+                    <div className = "dotted-line-down"/>
+                    <div className="create-job-input-2">
+                        <h3>test3</h3>
+                    </div>                   
+                </span>
             </div>
 
                 
