ea23cac719b4cef56598b38bb243cd79122fe5fd
gwee melvin <gwee_melvin@live.com>
2024-10-18 09:30:42 +0800

    added header and page status icon for create-job component

--- frontend/src/components/CreateJobComponent/CreateJobComponent.js
+++ frontend/src/components/CreateJobComponent/CreateJobComponent.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./CreateJobComponent.scss";
 import { styled, Paper, Button, Grid2 } from "@mui/material";
+import PageIcon from "../../assets/icons/page-icon.svg";
 
 class CreateJobComponent extends React.Component {
-
+  
 
   render() {
     return (
@@ -11,6 +12,11 @@ class CreateJobComponent extends React.Component {
             <div className="create-job-header">
                 <h1>Create a Job</h1>
                 <h4>Fill In Job Detail To Queue Or Run A New ReposenseCloud Job</h4>
+                <span className="create-job-page-status">
+                    <img src = {PageIcon} alt="Page Icon" className="page-icon1"/>
+                    <div className = "dotted-line"/>
+                    <img src = {PageIcon} alt="Page Icon" className="page-icon2"/>
+                </span>
             </div>
 
                 
