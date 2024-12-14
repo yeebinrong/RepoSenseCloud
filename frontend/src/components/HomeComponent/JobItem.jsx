541b72ab6d7e0c86dcd7219db3f4235ca7f0a6fc
Craby <lethuy1137@gmail.com>
2024-12-14 14:40:50 +0800

    fix table job item

--- frontend/src/components/HomeComponent/JobItem.jsx
+++ frontend/src/components/HomeComponent/JobItem.jsx
@@ -5,42 +5,40 @@ function JobItem({name, status, lastUpdated, nextScheduled, icon, view, edit, ru
     const statusClass = status.toLowerCase();
 
     return (
-        <table className={styles.jobTable}>
-            <tr className={styles.jobItem}>
-                <td className={styles.jobInfo}>
-                    <input type="checkbox" className={styles.checkBox}/>
-                    <span className={styles.jobName}>{name}</span>
-                </td>
-                <td className={`${styles.jobStatus} ${styles[statusClass]}`}>
-                    <div className={styles.statusIndicator}/>
-                    <span>{status}</span>
-                </td>
-                <td className={styles.jobTiming}>
-                    <div className={styles.timeInfo}>
-                        <span className={styles.time}>{lastUpdated.time}</span>
-                        <span className={styles.date}>{lastUpdated.date}</span>
-                    </div>
-                </td>
-                <td className={styles.jobTiming}>
-                    <div className={styles.timeInfo}>
-                        <span className={styles.time}>{nextScheduled.time}</span>
-                        <span className={styles.date}>{nextScheduled.date}</span>
-                    </div>
-                </td>
-                <td className={styles.jobActions}>
-                    <img src="view.svg" alt="View" className={styles.actionIcon}/>
-                    <img src="edit.svg" alt="Edit" className={styles.actionIcon}/>
-                    <img src="rerun.svg" alt="Run" className={styles.actionIcon}/>
-                </td>
-                <td>
 
-                    <button className={styles.moreOptionsButton}>
-                        <text className={styles.moreOptionsText}>. . .</text>
-                    </button>
-                </td>
-            </tr>
-            <hr/>
-        </table>
+        <tr className={styles.jobItem}>
+            <td className={styles.jobInfo}>
+                <input type="checkbox" className={styles.checkBox}/>
+                <span className={styles.jobName}>{name}</span>
+            </td>
+            <td className={`${styles.jobStatus} ${styles[statusClass]}`}>
+                <div className={styles.statusIndicator}/>
+                <span>{status}</span>
+            </td>
+            <td className={styles.jobTiming}>
+                <div className={styles.timeInfo}>
+                    <span className={styles.time}>{lastUpdated.time}</span>
+                    <span className={styles.date}>{lastUpdated.date}</span>
+                </div>
+            </td>
+            <td className={styles.jobTiming}>
+                <div className={styles.timeInfo}>
+                    <span className={styles.time}>{nextScheduled.time}</span>
+                    <span className={styles.date}>{nextScheduled.date}</span>
+                </div>
+            </td>
+            <td className={styles.jobActions}>
+                <img src="view.svg" alt="View" className={styles.actionIcon}/>
+                <img src="edit.svg" alt="Edit" className={styles.actionIcon}/>
+                <img src="rerun.svg" alt="Run" className={styles.actionIcon}/>
+            </td>
+            <td>
+
+                <button className={styles.moreOptionsButton}>
+                    <text className={styles.moreOptionsText}>. . .</text>
+                </button>
+            </td>
+        </tr>
     );
 }
 
