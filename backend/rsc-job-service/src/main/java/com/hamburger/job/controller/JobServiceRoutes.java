4d3be0f37984480d16eae79f82c287f6aec89ebe
gwee melvin <gwee_melvin@live.com>
2025-01-17 20:07:35 +0800

    HAMB-37 updated refactored endpoint to be consistent with other service

--- backend/rsc-job-service/src/main/java/com/hamburger/job/controller/JobServiceRoutes.java
+++ backend/rsc-job-service/src/main/java/com/hamburger/job/controller/JobServiceRoutes.java
@@ -1,5 +1,6 @@
 package com.hamburger.job.controller;
 import com.hamburger.job.objects.*;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -9,81 +10,82 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/jobs")
 public class JobServiceRoutes {
 
-    @GetMapping("/jobs")
+    @GetMapping("/")
     public getJobs() {
         System.out.println("retrieving jobs");
         return;
     }
 
-    @GetMapping("/jobs?page={page}&limit={limit}")
-    public getJobsByPage(@PathVariable int page, @PathVariable int limit) {
+    @GetMapping(params = {"page", "limit"})
+    public getJobsByPage(@RequestParam int page, @RequestParam int limit) {
         System.out.println("retrieving " + page + " of jobs with " + limit + " jobs per page");
         return;
     }
 
-    @GetMapping("/jobs/{id}")
+    @GetMapping("/{id}")
     public getJobById(@PathVariable int id) {
         System.out.println("retrieving job with id " + id);
         return;
     }
 
-    @GetMapping("/jobs/search/{keyword}")
+    @GetMapping("/search/{keyword}")
     public getJobsByKeyword(@PathVariable String keyword) {
         System.out.println("retrieving jobs with keyword " + keyword);
         return;
     }
 
-    @PostMapping("/jobs/save") //double check use case
+    @PostMapping("/save") //double check use case
     public saveJob() {
         System.out.println("saving all job");
         return;
     }
 
-    @PostMapping("/jobs/create")
+    @PostMapping("/create")
     public createJob(@RequestBody Job job) {
         System.out.println("creating job");
         return;
     }
 
-    @PostMapping("/jobs/edit/{jobId}")
+    @PostMapping("/edit/{jobId}")
     public editJob(@PathVariable int jobId, @RequestBody Job job) {
         System.out.println("editing job with id " + job-id);
         return;
     }
 
-    @PostMapping("/jobs/start/{jobId}")
+    @PostMapping("/start/{jobId}")
     public startJob(@PathVariable int jobId) {
         System.out.println("starting job with id " + jobId);
         return;
     }
 
-    @PatchMapping("/jobs/edit/{jobId}")
+    @PatchMapping("/edit/{jobId}")
     public updateJob(@PathVariable int jobId, @RequestBody Job job) {
         System.out.println("updating job with id " + jobId);
         return;
     }
 
-    @DeleteMapping("/jobs/delete/{jobId}")
+    @DeleteMapping("/delete/{jobId}")
     public deleteJob(@PathVariable int jobId) {
         System.out.println("deleting job with id " + jobId);
         return;
     }
 
-    @DeleteMapping("/jobs/delete-all")
+    @DeleteMapping("/delete-all")
     public deleteAllJobs() {
         System.out.println("deleting all jobs");
         return;
     }
 
-    @DeleteMapping("/jobs/delete-all-scheduled")
+    @DeleteMapping("/delete-all-scheduled")
     public deleteAllScheduledJobs() {
         System.out.println("deleting all upcoming scheduled jobs");
         return;
     }
 
-    @DeleteMapping("/jobs/delete-all-completed")
+    @DeleteMapping("/delete-all-completed")
     public deleteAllCompletedJobs() {
         System.out.println("deleting all completed jobs");
         return;
