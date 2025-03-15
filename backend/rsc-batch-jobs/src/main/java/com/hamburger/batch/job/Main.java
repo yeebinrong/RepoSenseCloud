5ad033db69501619267ba27ea3221e0d5d049733
Craby <lethuy1137@gmail.com>
2025-03-15 22:03:49 +0800

    add rsc-batch-jobs

---
+++ backend/rsc-batch-jobs/src/main/java/com/hamburger/batch/job/Main.java
@@ -0,0 +1,13 @@
+package com.hamburger.batch.job;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        if (args.length > 0) {
+            for (String arg : args) {
+                System.out.println(arg);
+
+            }
+        }
+    }
+}
