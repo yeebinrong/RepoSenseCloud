4d0d687b98d27b31551c6d5161edfcd3e7434330
Craby <lethuy1137@gmail.com>
2025-06-07 17:35:05 +0700

    merge with batch-job

--- backend/rsc-batch-job/src/main/java/com/hamburger/batch/scheduler/TaskDispatcherHandler.java
+++ backend/rsc-batch-job/src/main/java/com/hamburger/batch/scheduler/TaskDispatcherHandler.java
@@ -1,4 +1,4 @@
-package com.hamburger;
+package com.hamburger.batch.scheduler;
 
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.*;
